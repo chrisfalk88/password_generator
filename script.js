0af6616575bc09b008b4d279706d9d643f813271
Chris Falk <christophersfalk@gmail.com>
2020-08-24 15:14:08 -0700

    added arrays for character types. added a function to check password length.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Constants
 const generateBtn = document.querySelector("#generate");
+let specialChar = [" ", "!", "\"", "#", "$", "%", "&", "\'", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+let upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+let selectedCriteria = [];
+let inputLength = prompt("Please enter password length");
 
 // Functions
 
@@ -13,10 +19,19 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Function to take test user input between number min and max
+function lengthCheck() {
+  while (parseInt(inputLength) < 8 || parseInt(inputLength) > 128 ) {
+    inputLength = prompt("Please make your password inbetween 8 and 128 characters");
+    console.log(inputLength);
+  }
+}
+
+
 /**
  * generate a password based on certain criteria
  */
-function generatePassword(){
+function generatePassword() {
 
   // Password is currently blank! We need to make a better one
   let password = "";
