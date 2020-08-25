c8a4c0172794ed5551a8e18b82df0103665c224f
Chris Falk <christophersfalk@gmail.com>
2020-08-25 10:40:29 -0700

    wrote functions to combine arrays and then flatten them into one array. wrote code to randomly select characters.

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ let upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 let lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 let selectedCriteria = [];
+
 let inputLength = prompt("Please enter password length");
+lengthCheck();
+let inputUpper = confirm("Would you like Uppercase Letters?");
+let inputLower = confirm("Would you like Lowercase Letters?");
+let inputSpecial = confirm("Would you like Special Characters?");
+let inputNum = confirm("Would you like Numbers?");
 
 // Functions
 
@@ -21,28 +27,63 @@ function writePassword() {
 
 // Function to take test user input between number min and max
 function lengthCheck() {
-  while (parseInt(inputLength) < 8 || parseInt(inputLength) > 128 ) {
+  while (parseInt(inputLength) < 8 || parseInt(inputLength) > 128) {
     inputLength = prompt("Please make your password inbetween 8 and 128 characters");
-    console.log(inputLength);
   }
 }
 
+//Function to concat Upper to selected Critieria array
+function upperCheck() {
+  if (inputUpper) {
+    selectedCriteria.push(upperChar);
+  }
+}
+
+//Function to concat Lower to selected Critieria array
+function lowerCheck() {
+  if (inputLower) {
+    selectedCriteria.push(lowerChar);
+  }
+}
+
+//Function to concat Special to selected Critieria array
+function specialCheck() {
+  if (inputSpecial) {
+    selectedCriteria.push(specialChar);
+  }
+}
+
+//Function to concat Num to selected Critieria array
+function numCheck() {
+  if (inputNum) {
+    selectedCriteria.push(numChar);
+  }
+}
 
 /**
  * generate a password based on certain criteria
  */
 function generatePassword() {
 
+  upperCheck();
+  lowerCheck();
+  specialCheck();
+  numCheck();
+
+  let passwordCriteria = selectedCriteria.flat(1);
+
   // Password is currently blank! We need to make a better one
-  let password = "";
+  let password = ""; //proabbly put the math random on selected criteria here 
 
+  for (i = 0; i < parseInt(inputLength); i ++) {
+    password = password + (passwordCriteria[Math.floor((Math.random() * passwordCriteria.length) + 1)]);
+    
 
-
-
+  }
 
   return password;
 }
-
+generatePassword();
 // Main Process
 
 // Add event listener to generate button
