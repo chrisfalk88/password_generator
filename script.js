ecf4a68312cda6c35a08418258de2deb25ff9d0f
Chris Falk <christophersfalk@gmail.com>
2020-08-27 13:01:12 -0700

    removed firstClick function. moved input requests and input checks into generate password function. removed passwordCriteria array redunancy. consolidated input checks as one function with parameters.

--- script.js
+++ script.js
@@ -6,89 +6,86 @@ let lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 let numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 let selectedCriteria = [];
 
-
+let inputLength;
+let inputUpper;
+let inputLower;
+let inputSpecial;
+let inputNum;
 
 // Functions
 
 // Function to take test user input between number min and max
 function lengthCheck() {
-  while (parseInt(inputLength) < 8 || parseInt(inputLength) > 128) {
+  while (parseInt(inputLength) < 8 || parseInt(inputLength) > 128 || inputLength ===  null) {
     inputLength = prompt("Please make your password inbetween 8 and 128 characters");
   }
 }
 
 //Function to concat Upper to selected Critieria array
-function upperCheck() {
-  if (inputUpper) {
-    selectedCriteria.push(upperChar);
+
+function inputCheck(inputType, typeArray) {
+  if (inputType) {
+    selectedCriteria.push(typeArray);
   }
 }
 
-//Function to concat Lower to selected Critieria array
-function lowerCheck() {
-  if (inputLower) {
-    selectedCriteria.push(lowerChar);
-  }
-}
-
-//Function to concat Special to selected Critieria array
-function specialCheck() {
-  if (inputSpecial) {
-    selectedCriteria.push(specialChar);
-  }
-}
-
-//Function to concat Num to selected Critieria array
-function numCheck() {
-  if (inputNum) {
-    selectedCriteria.push(numChar);
-  }
-}
 
 // Function that generates the password 
 function generatePassword() {
 
-  // Run all the functions to determine what criteria we need to include
-  upperCheck();
-  lowerCheck();
-  specialCheck();
-  numCheck();
+  // checks confirm values to be true. adds to selectedCriteria 
 
-  // add all flagged arrays into one array
-  let passwordCriteria = selectedCriteria.flat(1);
+  inputLength = prompt("Please enter password length");
+  lengthCheck();
+  inputUpper = confirm("Would you like Uppercase Letters?");
+  inputLower = confirm("Would you like Lowercase Letters?");
+  inputSpecial = confirm("Would you like Special Characters?");
+  inputNum = confirm("Would you like Numbers?");
+
+  while (!inputUpper && !inputLower && !inputSpecial && !inputNum ) {
+    alert("You need to select some criteria")
+    inputUpper = confirm("Would you like Uppercase Letters?");
+    inputLower = confirm("Would you like Lowercase Letters?");
+    inputSpecial = confirm("Would you like Special Characters?");
+    inputNum = confirm("Would you like Numbers?");
+  }
+
+
+  // Run all the functions to determine what criteria we need to include
+
+inputCheck(inputUpper, upperChar);
+inputCheck(inputLower, lowerChar);
+inputCheck(inputSpecial, specialChar);
+inputCheck(inputNum, numChar);
+
+
+  
+  selectedCriteria = selectedCriteria.flat(1);
 
   // Password is currently blank! We need to make a better one
-  let password = ""; 
+  let password = "";
 
   // for loop that generates the password
-  for (i = 0; i < parseInt(inputLength); i ++) {
-    password = password + (passwordCriteria[Math.floor((Math.random() * passwordCriteria.length))]);
-
+  for (i = 0; i < parseInt(inputLength); i++) {
+    password = password + (selectedCriteria[Math.floor((Math.random() * selectedCriteria.length))]);
   }
 
   return password;
 }
 
+
 // Writes the password to the text field in the HTML
 function writePassword() {
+  // check critiera here? or even get rid of the firstclick function and check it here 
+
+
   const password = generatePassword();
   const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-
-
 // Main Process
-
-let inputLength = prompt("Please enter password length");
-lengthCheck();
-let inputUpper = confirm("Would you like Uppercase Letters?");
-let inputLower = confirm("Would you like Lowercase Letters?");
-let inputSpecial = confirm("Would you like Special Characters?");
-let inputNum = confirm("Would you like Numbers?");
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
